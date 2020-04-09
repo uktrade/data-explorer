016f987c0d00b659e717987bd0d6d8272ec01305
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2020-04-09 13:32:00 +0100

    Debug tool bar

--- data_explorer/urls.py
+++ data_explorer/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('explorer/', include('explorer.urls')),
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
