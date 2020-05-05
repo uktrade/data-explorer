1df2c5d54fbce08a0b277d2fec9317fb9917c7d8
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2020-05-05 13:40:58 +0100

    Gov uk style kit added

--- scripts/start_cf.sh
+++ scripts/start_cf.sh
@@ -3,8 +3,6 @@
 source ./scripts/functions.sh
 
 run "python manage.py migrate --noinput"
-run "python manage.py collectstatic --noinput"
-
 
 if [[ -z "${DEVELOPMENT_SERVER}" ]]; then
     run "waitress-serve --port=$PORT data_explorer.wsgi:application"
---
+++ webpack.base.config.js
@@ -0,0 +1,39 @@
+const path = require("path")
+const webpack = require('webpack')
+const BundleTracker = require('webpack-bundle-tracker')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const glob = require("glob");
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    index: './static/js/index', // entry point of our app. static/js/index.js should require other js modules and dependencies it needs
+    defaultVueApp: './static/js/vue/apps/default-app',
+  },
+  output: {
+      path: path.resolve(__dirname, './static/js/bundles'),
+      filename: "[name]-[hash].js",
+  },
+  module: {
+    rules: [
+      { test: /\.vue$/, loader: 'vue-loader'},
+      { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader'},
+      { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
+    ],
+  },
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+    new VueLoaderPlugin(),
+    new CopyPlugin([
+      { from: path.resolve(__dirname, './node_modules/govuk-frontend/all.js'), to: path.resolve(__dirname, './static/js/') },
+    ]),
+  ],
+  resolve: {
+    modules: ['node_modules', 'bower_components'],
+    extensions: ['.js'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+}
