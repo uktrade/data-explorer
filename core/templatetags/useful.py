cca79dd54ed036a2fcd1339271ce3834150e1901
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2020-05-11 16:06:53 +0100

    fixed active link

--- core/templatetags/useful.py
+++ core/templatetags/useful.py
@@ -3,8 +3,28 @@ from django.conf import settings
 
 register = template.Library()
 
+HOME_MENU_ITEM = 'home'
+LOGS_MENU_ITEM = 'logs'
+NEW_MENU_ITEM = 'new_query'
+PLAY_MENU_ITEM = 'play'
+TABLE_BROWSER_MENU_ITEM = 'table_browser'
+
 
 @register.simple_tag
 def is_table_browser_enabled():
     return settings.ENABLE_TABLE_BROWSER
 
+
+@register.simple_tag(takes_context=True)
+def get_active_menu(context):
+    view_name = context['request'].resolver_match.url_name
+    if view_name in ['explorer_index', 'query_detail']:
+        return HOME_MENU_ITEM
+    if view_name == 'explorer_logs':
+        return LOGS_MENU_ITEM
+    if view_name == 'explorer_play':
+        return PLAY_MENU_ITEM
+    if view_name == 'query_create':
+        return NEW_MENU_ITEM
+    if view_name in ['connection_browser_list', 'table_browser_list', 'table_browser_detail']:
+        return TABLE_BROWSER_MENU_ITEM
