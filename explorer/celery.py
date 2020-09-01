e7b937b42cfa4105c564aa050d1b1ca348a8f5c0
Kristof Geebelen <geebelenk@gmail.com>
2020-09-01 14:00:12 +0100

    feature: scheduled schema build
    In a multiuser deployment, the schema building is triggered when the user clicks on show schema.
    When there are many tables this can take a while. We change the triggering to a scheduled task to populate
    the cache every 8 minutes and the cache will expire every 10 minutes so the schema is quite up to date and
    is always availble for quick retrieval from the cache.
    
    In data workspace we assume the schema doesn't change during a user session as the user permissions cannot change.
    So the cache is set on the start of the session and does not expire.

--- explorer/celery.py
+++ explorer/celery.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
+from datetime import timedelta
 
 from celery import Celery
+from django.conf import settings
 
-# set the default Django settings module for the 'celery' program.
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'explorer.settings.base')
 
 app = Celery('explorer')
@@ -13,3 +15,16 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {}
+
+for alias in settings.EXPLORER_CONNECTIONS:
+    app.conf.beat_schedule.update(
+        {
+            f"trigger-build-schema-{alias}": {
+                "task": "explorer.tasks.build_schema_cache_async",
+                "schedule": timedelta(minutes=8),
+                "args": (alias,),
+            }
+        }
+    )
